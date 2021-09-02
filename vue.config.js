551f95025ede7807f63658d5d5249411f66f7d39
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-09-02 10:04:29 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     lintOnSave: false,
     chainWebpack: (config) => {
       config.plugin("html").tap((args) => {
-        args[0].title = "Quotify - Search and Copy Quotes";
+        args[0].title = "QuoteMaster - Quotes and more quotes";
         return args;
       });
     },
